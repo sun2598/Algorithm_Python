d140e26125f21485f9a6b07865d622b7e34de738
sun2598 <cksun2598@gmail.com>
2023-08-10 12:20:41 +0900

    23/08/09 solved 03_05

--- Section03_탐색&시뮬레이션(string,1,2차원리스트탐색)/03_05_수들의 합.py
+++ Section03_탐색&시뮬레이션(string,1,2차원리스트탐색)/03_05_수들의 합.py
@@ -28,32 +28,33 @@ lt = 0
 rt = 1
 
 # <two pointer algorithm> 시간복잡도 O(n)
-# sum : lt ~ rt 까지의 부분수열의 합. 처음엔 list[0] 대입
-# 1  2  1  3  1  1  1  2   -> sum = 1, sum < m인지 판별 후 맞으면 rt++
-# lt
-# rt
-# 1  2  1  3  1  1  1  2   -> sum = 3, sum < m인지 판별 후 맞으면 rt++
+# sum : lt ~ rt(rt 불포함⭐️) 까지의 부분수열의 합. 처음엔 list[0] 대입
+# 1  2  1  3  1  1  1  2   -> sum = 1, sum < m이니까 rt += 1
 # lt rt
-# 1  2  1  3  1  1  1  2   -> sum = 7, sum > m인지 판별 후 맞으면 lt++(이제 sum==m이 될 일은 없으니까)
-# lt       rt
-# 1  2  1  3  1  1  1  2   -> sum = 6, sum == m인지 판별 후 맞으면 lt++(이제 sum==m이 될 일은 없으니까)
+# 1  2  1  3  1  1  1  2   -> sum = 3, sum == m이니까 lt += 1(이제 sum==m이 될 일은 없으니까)
+# lt    rt
+# 1  2  1  3  1  1  1  2   -> sum = 2, sum < m이니까 rt += 1
+#    lt rt
+# 1  2  1  3  1  1  1  2   -> sum = 3, sum == m이니까 lt += 1(이제 sum==m이 될 일은 없으니까)
 #    lt    rt
-# 1  2  1  3  1  1  1  2   -> sum = 4, sum < m인지 판별 후 맞으면 rt++
+# 1  2  1  3  1  1  1  2   -> sum = 1, sum < m이니까 rt += 1
 #       lt rt
-# => 따라서 sum < m 이면 rt++ / sum >= m 이면 lt++
+# 1  2  1  3  1  1  1  2   -> sum = 4, sum > m이니까 lt += 1
+#       lt    rt
+# => 따라서 sum < m 이면 rt += 1 / sum >= m 이면 lt += 1
 
-while True: # break 만날때까지 무한반복
+while True: # break 만날때까지 무한반복 (여기선 rt >= n 이 돼버리면 break)
     if sum < m:
         if rt < n:
-            sum += list[rt]
-            rt += 1
+            sum += list[rt] # 먼저 현재의 list[rt]를 sum에 더해주고
+            rt += 1 # 그 다음 오른쪽으로 한칸
         else:
             break
     elif sum == m:
         cnt += 1
         sum -= list[lt]
         lt += 1
-    else:
+    else: # sum > m 인 경우
         sum -= list[lt]
         lt += 1
 
